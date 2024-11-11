11b1be1c2423c88bbc2e2c1dc750ae3d6357e6eb
afranquez12 <163781509+afranquez12@users.noreply.github.com>
2024-11-11 11:27:12 -0800

    Error 1

---
+++ src/CSC450_CTA1_ERR1.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : CSC450_CTA1_ERR1.cpp
+// Author      : Abnel Franquez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>   // include correct header for input-output
+using namespace std;  // corrected the namespace declaration
+
+// Main function
+int main()
+{
+    // Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl; // corrected missing quotes around the text
+
+    // Wait for output screen (cross-platform)
+    cin.get();  // waits for the user to press Enter
+
+    // Main function return statement
+    return 0;
+}
